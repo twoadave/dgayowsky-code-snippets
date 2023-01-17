bd320a9959b41180636fb104efe571b2d9989506
twoadave <gayowsky.d@gmail.com>
2023-01-17 12:20:10 -0500

    Qiskit circuit basics.

---
+++ qiskit-tutorials/circuit_basics.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 12 11:32:26 2022
+
+@author: David
+
+Attempt #2 at creating qiskit tutorial...
+"""
+
+#######################################################################
