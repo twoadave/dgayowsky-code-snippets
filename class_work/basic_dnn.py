75a472c032cf2fa2534b24a725ca1c42f174ee36
twoadave <gayowsky.d@gmail.com>
2023-03-06 14:10:35 -0500

    Start file do do do

---
+++ class_work/basic_dnn.py
@@ -0,0 +1,15 @@
+"""
+Created on Mon Mar 6 2023
+
+@author: David Gayowsky
+
+Create a little artificial neural network from scratch.
+"""
+
+#######################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import copy
+import itertools
