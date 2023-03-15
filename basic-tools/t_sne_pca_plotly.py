a4ccf936e093abd63056c9056f5612a279659cf2
twoadave <gayowsky.d@gmail.com>
2023-03-15 12:04:02 -0400

    Mmmmm t-sne mmmmm

---
+++ basic-tools/t_sne_pca_plotly.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 2023
+
+@author: David
+
+Practice t-SNE and maybe PCA dimensionality reduction using Plotly.
+"""
+
+#######################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import copy
+import itertools
+
+import plotly.express as px
