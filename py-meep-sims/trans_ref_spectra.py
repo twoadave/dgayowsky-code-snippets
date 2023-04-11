fc04b14d603252fab7239502c842db0264836a38
twoadave <gayowsky.d@gmail.com>
2023-04-11 17:39:07 -0400

    Transmittance and relfectance spectra...

---
+++ py-meep-sims/trans_ref_spectra.py
@@ -0,0 +1,17 @@
+"""
+Created on Fri Mar 31 2023
+
+@author: David Gayowsky
+
+PyMeep tutorials, transmittance and reflectance spectra.
+"""
+
+#######################################################################
+
+import meep as mp
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython.display import Video 
+import os
+
+#######################################################################
