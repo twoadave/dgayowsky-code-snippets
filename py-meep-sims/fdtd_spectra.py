0706f3457e99652e29a6a04a8f9ab1c1714b3452
twoadave <gayowsky.d@gmail.com>
2023-04-12 11:27:16 -0400

    fdtd spectra

---
+++ py-meep-sims/fdtd_spectra.py
@@ -0,0 +1,17 @@
+"""
+Created on Fri Mar 31 2023
+
+@author: David Gayowsky
+
+FDTD transmission spectra, tiny3d -> pymeep.
+"""
+
+#######################################################################
+
+import meep as mp
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython.display import Video 
+import os
+
+#######################################################################
