6c4cebd140743f726a42357b5f59062655d30ad2
twoadave <gayowsky.d@gmail.com>
2023-04-12 14:12:07 -0400

    not rlly working

--- py-meep-sims/fdtd_spectra.py
+++ py-meep-sims/fdtd_spectra.py
@@ -29,7 +29,7 @@ geometry = [mp.Block(mp.Vector3(mp.inf,mp.inf, 0.5),
                      center=mp.Vector3(0,0,0),
                      material=mp.Medium(epsilon=12))]
 
-nfreq = 5  # number of frequencies at which to compute flux
+nfreq = 25  # number of frequencies at which to compute flux
 
 ###############################################################
 
@@ -37,12 +37,15 @@ nfreq = 5  # number of frequencies at which to compute flux
 freq_range = (200e12, 400e12)
 freq0 = np.sum(freq_range)/2
 lambda0 = c/freq0
+print(lambda0)
 
-fcen = 1/lambda0 # pulse center frequency
+#fcen = 1/lambda0 # pulse center frequency
+fcen = 1 #Corresponds to 300THz
+#print(fcen)
 
 ###############################################################
 
-df = 0.1     # pulse width (in frequency)
+df = 0.75   # pulse width (in frequency)
 
 pml_layers = [mp.PML(thickness=0.1,direction=mp.Z)]
 
@@ -64,17 +67,23 @@ sim = mp.Simulation(cell_size=cell,
 
 #Now want to put in our flux calculation...
 
+# reflected flux
+refl_fr = mp.FluxRegion(center=mp.Vector3(0,0,-1), size=mp.Vector3(2,2,0))
+refl = sim.add_flux(fcen, df, nfreq, refl_fr)
+
 # transmitted flux
 tran_fr = mp.FluxRegion(center=mp.Vector3(0,0,1), size=mp.Vector3(2,2,0))
 tran = sim.add_flux(fcen, df, nfreq, tran_fr)
 
 pt = mp.Vector3(0,0,1)
 
-sim.run(until=25)
+sim.run(until=500)
 
 # save incident power for transmission plane
 straight_tran_flux = mp.get_fluxes(tran)
 flux_freqs = mp.get_flux_freqs(tran)
+# for normalization run, save flux fields data for reflection plane
+straight_refl_data = sim.get_flux_data(refl)
 
 wl = []
 Ts = []
@@ -83,11 +92,12 @@ for i in range(nfreq):
     Ts = np.append(Ts,straight_tran_flux[i])
 
 print(Ts)
+print(wl)
 
 if mp.am_master():
     plt.figure()
     plt.plot(wl,Ts,'ro-',label='transmittance')
-    plt.axis([5.0, 10.0, 0, 1])
+    #plt.axis([5.0, 10.0, 0, 1])
     plt.xlabel("wavelength (μm)")
     plt.legend(loc="upper right")
     plt.show()
