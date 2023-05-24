b646b5695af7e4d88efc084d6836e0a2a18a5093
twoadave <gayowsky.d@gmail.com>
2023-05-24 11:04:07 -0400

    misc stuff

--- py-meep-sims/lens_sim.py
+++ py-meep-sims/lens_sim.py
@@ -92,9 +92,23 @@ def plot_intensity(in_data, run_time):
 
     in_data = np.rot90(in_data)
 
+    major_ticks = np.arange(0, 601, 100)
+    minor_ticks = np.arange(0, 601, 5)
+
+    major_yticks = np.arange(0, 321, 100)
+    minor_yticks = np.arange(0, 321, 5)
+
+
     plt.imshow(in_data, aspect = 'equal', interpolation='none', origin='upper', norm="logit")
+
+    plt.xticks(major_ticks)
+    plt.xticks(minor_ticks, minor=True)
+    plt.yticks(major_yticks)
+    plt.yticks(minor_yticks, minor=True)
+
     plt.colorbar(label="\n Electric Field Intensity")
     plt.title("Electric Field Intensity at Time " + str(run_time) + " [Meep Time] \n")
+    plt.grid(which='both')
     plt.show()
 
 #Create some basic test shapes to see if we can get an idea of what we want our lens to look like:
@@ -170,6 +184,54 @@ def basic_shapes_test_sim(run_time):
 
     return ez_data, ey_data, ex_data
 
+#Try and recreate our shape on a basic grid.
+def grid_shapes_test(run_time):
+
+    resolution = 10 # pixels/um
+
+    sx = 60  # size of cell in X direction
+    sy = 32  # size of cell in Y direction
+    cell = mp.Vector3(sx,sy,0)
+
+    dpml = 1.0
+    pml_layers = [mp.PML(dpml)]
+
+    fcen = 0.15  # pulse center frequency
+    df = 0.1     # pulse width (in frequency)
+
+    #Create a simple point source
+    sources = [mp.Source(mp.GaussianSource(fcen,fwidth=df),
+                        component=mp.Ez,
+                        center=mp.Vector3(-1*(0.5*sx)+dpml,0,0)),
+                mp.Source(mp.GaussianSource(fcen,fwidth=df),
+                        component=mp.Ey,
+                        center=mp.Vector3(-1*(0.5*sx)+dpml,0,0)),
+                mp.Source(mp.GaussianSource(fcen,fwidth=df),
+                        component=mp.Ex,
+                        center=mp.Vector3(-1*(0.5*sx)+dpml,0,0))]
+    
+    geometry = [mp.Ellipsoid(size=mp.Vector3(10,sy,mp.inf),
+                     center=mp.Vector3(0,0,0),
+                     material=mp.Medium(epsilon=15))]
+
+    sim = mp.Simulation(cell_size=cell,
+                        boundary_layers=pml_layers,
+                        geometry=geometry,
+                        sources=sources,
+                        resolution=resolution)
+
+    plt.figure(dpi=100)
+    sim.plot2D()
+    plt.show()
+
+    sim.run(until=run_time)
+
+    ez_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Ez)
+    ey_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Ey)
+    ex_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Ex)
+
+    return ez_data, ey_data, ex_data
+
 #######################################################################
 
 #Main: Let's run some functions!
@@ -178,7 +240,3 @@ run_time = 75
 ez_data, ey_data, ex_data = basic_shapes_test_sim(run_time)
 in_data = intensity_threevec(ex_data, ey_data, ez_data)
 plot_intensity(in_data, run_time)
-
-#print(ez_data)
-#print(ey_data)
-#print(ex_data)
