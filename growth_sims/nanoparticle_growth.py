3e938406e7bfb4c28275598bd178b78ff642ce0c
twoadave <gayowsky.d@gmail.com>
2023-05-30 10:55:38 -0400

    start

---
+++ growth_sims/nanoparticle_growth.py
@@ -0,0 +1,9 @@
+"""
+Created on Fri May 26
+
+@author: David Gayowsky
+
+Simple growth Monte Carlo learning algorithm.
+"""
+
+#######################################################################
