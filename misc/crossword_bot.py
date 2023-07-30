2bf94a581e94f8460ce730f9dfcd32980dae0555
twoadave <gayowsky.d@gmail.com>
2023-07-30 14:33:07 -0400

    working kinda

--- misc/crossword_bot.py
+++ misc/crossword_bot.py
@@ -24,7 +24,7 @@ intents.message_content = True
 
 cdescription = '''A crossword bot to allow you to play crosswords with your friends.'''
 
-bot = commands.Bot(command_prefix='$c', description=cdescription, intents=intents)
+bot = commands.Bot(command_prefix='&', description=cdescription, intents=intents)
 
 #Bot login.
 @bot.event
@@ -32,9 +32,17 @@ async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
+#Check function to make sure we're only in the allowed channel.
+def in_channel(*channels):
+    def predicate(ctx):
+        return ctx.channel.id in channels
+    return commands.check(predicate)
+
 #Define a bot command to set a new crossword puzzle.
+#Check to make sure we're only in the allowed channel.
+@in_channel(1135273922649133176)
 @bot.command(description='Set a crossword puzzle.')
-async def crossword(self, ctx, clue, numletters: int, answer):
+async def crossword(ctx, clue, numletters: int, answer):
     #Delete the message so nobody can see the answer.
     await ctx.message.delete()
     #Declare the challenge to the channel.
@@ -58,30 +66,30 @@ async def crossword(self, ctx, clue, numletters: int, answer):
     while answer_guessed == 0:
 
         #Wait for a reply, make sure that the reply we're getting is a reply to the bot's message:
-        user_ans = await self.bot.wait_for('message', check=check)
+        user_ans = await bot.wait_for('message', check=check)
 
         #If they've guessed the answer correctly:
         if user_ans.content == answer:
             answer_guessed = 1
             #Congrats!
-            user_ans.add_reaction("✅")
+            await user_ans.add_reaction("✅")
             await ctx.send(f":tada: Congratulations {user_ans.author.mention}! :tada: You solved the crossword clue! The answer was {answer}.")
             #Ask for assists:
             assists_msg = await ctx.send(f"Did anyone help you guess? Please reply to this message with your assist. :hockey:")
 
             try:
-                user_assists_msg = await self.bot.wait_for('message', timeout=120.0, check=check_assists)
+                user_assists_msg = await bot.wait_for('message', timeout=120.0, check=check_assists)
             except asyncio.TimeoutError:
                 await ctx.send(f"Timeout! You didn't declare any assists. Thanks for playing!")
             else:
-                assist_member = user_assists_msg.mentions.members.first()
+                assist_member = user_assists_msg.mentions
                 await ctx.send(f"Assist accepted! Thanks {assist_member.mention} for the flow, the snow, and the apple :sunglasses: :hockey:")
 
         else:
-            user_ans.add_reaction("❌")
+            await user_ans.add_reaction("❌")
 
 #######################################################################
 
 #client = MyClient(intents=intents)
 
-bot.run(os.getenv('TOKEN'))
+bot.run('MTEzNTAwMjY1Nzk5NjY3MzA4NQ.GIPmPX.eCduy3bUyodCRHIH0XETh8_3UOMIxEUG7qG1T0')
