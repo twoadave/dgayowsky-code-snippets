a5bd1c2a8aa26f90a7299a5b74545afb8e3ff1c6
twoadave <gayowsky.d@gmail.com>
2023-07-30 13:41:33 -0400

    update to give incorrect and correct answers

--- misc/crossword_bot.py
+++ misc/crossword_bot.py
@@ -40,44 +40,45 @@ async def crossword(self, ctx, clue, numletters: int, answer):
     #Declare the challenge to the channel.
     init_message = await ctx.send(f"{ctx.author.mention} has declared a new crossword challenge! Your challenge is: \n {clue}, {numletters} letters.")
 
-    #Check that our answer is correct and in our channel, and is a reply to the bot.
+    #Check that our answer is a reply to the bot.
     def check(m):
         if m.reference is not None:
-            if m.reference.message_id == init_message.id and m.content == answer:
+            if m.reference.message_id == init_message.id:
                 return True
         return False
     
-    #Wait for a reply, make sure that the reply we're getting is a reply to the bot's message and contains the answer:
-    user_ans = await self.bot.wait_for('message', check=check)
-
     def check_assists(m):
         if m.reference is not None:
             if m.reference.message_id == assists_msg.id and m.author == user_ans.author and m.mentions:
                 return True
         return False
+    
+    answer_guessed = 0
+    
+    while answer_guessed == 0:
 
-    #If they've guessed the answer correctly:
-    if user_ans == answer:
-        #Congrats!
-        await ctx.send(f":tada: Congratulations {user_ans.author.mention}! :tada: You solved the crossword clue! The answer was {answer}.")
-        #Ask for assists:
-        assists_msg = await ctx.send(f"Did anyone help you guess? Please reply to this message with your assist. :hockey:")
+        #Wait for a reply, make sure that the reply we're getting is a reply to the bot's message:
+        user_ans = await self.bot.wait_for('message', check=check)
 
-        try:
-            user_assists_msg = await self.bot.wait_for('message', timeout=120.0, check=check_assists)
-        except asyncio.TimeoutError:
-            await ctx.send(f"Timeout! You didn't declare any assists. Thanks for playing!")
+        #If they've guessed the answer correctly:
+        if user_ans.content == answer:
+            answer_guessed = 1
+            #Congrats!
+            user_ans.add_reaction("✅")
+            await ctx.send(f":tada: Congratulations {user_ans.author.mention}! :tada: You solved the crossword clue! The answer was {answer}.")
+            #Ask for assists:
+            assists_msg = await ctx.send(f"Did anyone help you guess? Please reply to this message with your assist. :hockey:")
+
+            try:
+                user_assists_msg = await self.bot.wait_for('message', timeout=120.0, check=check_assists)
+            except asyncio.TimeoutError:
+                await ctx.send(f"Timeout! You didn't declare any assists. Thanks for playing!")
+            else:
+                assist_member = user_assists_msg.mentions.members.first()
+                await ctx.send(f"Assist accepted! Thanks {assist_member.mention} for the flow, the snow, and the apple :sunglasses: :hockey:")
+
         else:
-            assist_member = user_assists_msg.mentions.members.first()
-            await ctx.send(f"Assist accepted! Thanks {assist_member.mention} for the flow, the snow, and the apple :sunglasses: :hockey:")
-
-
-#Define a bot command to try and answer the crossword puzzle.
-'''@bot.command(description='Answer the current crossword puzzle.')
-async def ans(ctx, userans):
-    if userans == answer:
-        return'''
-
+            user_ans.add_reaction("❌")
 
 #######################################################################
 
